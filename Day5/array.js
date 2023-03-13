f9f22fe442b491a4099eeb29536ca859bc0742e3
adfar-136 <adfarrasheed136@gmail.com>
2023-03-13 20:46:19 +0530

    done

---
+++ Day5/array.js
@@ -0,0 +1,68 @@
+// var arr = [10,20,30,40,50]
+// console.log(arr[3])
+// var num = new Array()
+// num[0] = 10
+// num[1] = 20
+// num[2] = 30
+// num[3] = 40
+// console.log(num)
+// var num = new Array(1,20,30,40)
+// console.log(num)
+// var num = new Array(5)
+// console.log(num)
+// var arr = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ]
+// console.log(arr[1][2])
+// var arr = [
+//     1,2,[3,4,5],"adfar",[33,44,55,65]
+// ]
+// console.log(arr[4][2])
+
+// var arr = [12,34,56,76,67]
+// console.log(arr)
+// arr.push(123,342,"a")
+// console.log(arr)
+// var arr = [
+//     [1,2,3,4],
+//     [5,6,7,8],
+//     [43,54,65]
+// ]
+// arr.push([3,4,5,6])
+// console.log(arr)
+// arr[1].push("a")
+// console.log(arr)
+
+// var arr = [2,434,3,3,234,54]
+// var arr1 = arr.pop()
+// console.log(arr1)
+
+// var arr = [1,2,3,4,5,6,7,8,9,0]
+// var b = arr.slice(2,6)
+// console.log(b)
+
+// var arr = [1,2,3,4,5,6,7,8,9,0]
+// // arr.splice(2,2,'a',"b",3,443,4,534,43,543,44)
+// // var a= arr.splice(2)
+//  arr.splice(3,0,"Adfar")
+// console.log(arr)
+
+// var arr = [1,2,3,4,5,6,7,8,9,0]
+// console.log(arr.shift())
+// console.log(arr)
+
+// var arr = [1,2,3,4,5,6,7,8,9,0]
+// arr.unshift("a","c","e")
+// console.log(arr)
+
+// var arr = [1,2,3,4,5,6,7,8,9,0]
+// console.log(arr.indexOf(78))
+
+var arr1 =[1,2,3]
+var arr2= [4,5,6]
+var arr3 = [7,8,9]
+// var c = arr1.concat(arr2,arr3)
+var c = [].concat(arr1,arr3,arr2)
+console.log(c)
